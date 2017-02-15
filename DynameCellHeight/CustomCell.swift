e834100bb96a692e0edd008a4756b6523d5e358b
Dexter Juntilla <dexter@dnamicro.com>
2017-02-15 09:26:27 +0800

    working

---
+++ DynameCellHeight/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  DynameCellHeight
+//
+//  Created by DNA on 2/15/17.
+//  Copyright © 2017 DNA. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+}
--- DynameCellHeight/ViewController.swift
+++ DynameCellHeight/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+/*!
+ IMPORTANT
+ Set UILabel lines to 0
+ */
+class ViewController: UITableViewController {
+    
+    let arrayData = [
+        "Lorem ipsum dolor sit amet, ea his bonorum omittantur dissentiunt.",
+        "Lorem ipsum dolor sit amet, ea his bonorum omittantur dissentiunt. An vim habeo quaerendum, qui ubique consetetur et. In mel novum oratio vocibus, ad quot noluisse nec.",
+        "Lorem ipsum dolor sit amet, ea his bonorum omittantur dissentiunt. An vim habeo quaerendum, qui ubique consetetur et. In mel novum oratio vocibus, ad quot noluisse nec. Est at populo scaevola hendrerit, timeam delenit sed id. At mel quot audiam volumus. No est consul urbanitas, erat porro vel ut. Ad nec labitur vocibus, utinam populo delicata ad has.",
+        "Lorem ipsum dolor sit amet, ea his bonorum omittantur dissentiunt. An vim habeo quaerendum, qui ubique consetetur et. In mel novum oratio vocibus, ad quot noluisse nec. Est at populo scaevola hendrerit, timeam delenit sed id. At mel quot audiam volumus. No est consul urbanitas, erat porro vel ut. Ad nec labitur vocibus, utinam populo delicata ad has. Altera explicari vel an, qui ei diam noster latine, nihil euripidis cu has. No nec iisque mandamus partiendo. Usu ex probo explicari voluptaria. Vidit aliquip similique mea ne, vim semper voluptua electram no. Vim detracto insolens maluisset te."
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: - UITableViewDataSource, UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "CellIdentifier"
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! CustomCell
+        cell.label.text = self.arrayData[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
 }
 
